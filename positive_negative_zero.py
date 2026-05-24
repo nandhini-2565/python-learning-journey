a410841c4625d3d80baa3931e312bf19e7ca7ff6
NANDHINI <n98065017@gmail.com>
2026-05-24 15:41:29 +0530

    positive, negative, or zero checker

---
+++ positive_negative_zero.py
@@ -0,0 +1,10 @@
+# Program to check weather a number is positive, negatuve or zero
+
+n = int(input("Enter a number: "))
+
+if n > 0:
+  print(n, "is Positive")
+elif n < 0:
+  print(n, "is Negative")
+else:
+  print(n, "is Zero")
